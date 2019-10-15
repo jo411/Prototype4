06c63f5a7f42057c67c9ef313c508ae2d5de8fd7
Josh Nelson <josh.nelson1@yahoo.com>
2019-10-15 17:56:54 -0600

    Initial conveyor belts

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -9,7 +9,7 @@ public class ConveyorBelt : MonoBehaviour
 
     private HashSet<string> allowedTags = new HashSet<string>();
     public float moveSpeed = 1f;
-    public Vector3 moveDir = Vector3.zero;//direction to move in    
+    public Vector3 moveTo = Vector3.zero;//direction to move in    
     public HashSet<GameObject> targets = new HashSet<GameObject>();   
     public List<GameObject> deleteMark = new List<GameObject>();
 
@@ -20,21 +20,37 @@ public class ConveyorBelt : MonoBehaviour
         {
             allowedTags.Add(current);
         }
-
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        processRemoveBuffer();
         foreach (GameObject current in targets)
         {
-            if (current.GetComponent<ObjectData>().gameObjectData["Moving"] == this.gameObject)
+            ObjectData data = current.GetComponent<ObjectData>();
+            if (data == null) { return; }
+            if (data.gameObjectData.ContainsKey("Moving") && data.gameObjectData["Moving"] == this.gameObject)
             {
-                current.transform.position = Vector3.MoveTowards(current.transform.position, new Vector3(transform.position.x + moveDir.x, current.transform.position.y, transform.position.z + moveDir.z), Time.deltaTime * moveSpeed);
-            }else if(current.GetComponent<ObjectData>().gameObjectData["Moving"]=null)
+                current.transform.position = Vector3.MoveTowards(current.transform.position, new Vector3(transform.position.x + moveTo.x, current.transform.position.y, transform.position.z + moveTo.z), Time.deltaTime * moveSpeed);
+                //if(moveTo.x != 0)
+                //{
+                //    if (transform.position.x + moveTo.x == current.transform.position.x)
+                //    {
+                //      // deleteMark.Add(current.gameObject);
+                //    }
+                //}else
+                //{
+                //    if (transform.position.z + moveTo.z == current.transform.position.z)
+                //    {
+                //       // deleteMark.Add(current.gameObject);
+                //    }
+                //}
+            }else if(data.gameObjectData.ContainsKey("Moving"))
             {
-
+               // current.GetComponent<ObjectData>().gameObjectData["Moving"] = this.gameObject;
+                current.transform.position = Vector3.MoveTowards(current.transform.position, new Vector3(transform.position.x + moveTo.x, current.transform.position.y, transform.position.z + moveTo.z), Time.deltaTime * moveSpeed);
             }
         }
     }
@@ -42,14 +58,44 @@ public class ConveyorBelt : MonoBehaviour
     {
         targets.Add(collision.gameObject);
         ObjectData data = collision.gameObject.GetComponent<ObjectData>();
-        if (data!=null && allowedTags.Contains(collision.gameObject.tag) && data.gameObjectData.ContainsKey("Moving"))
+        if(data != null && data.gameObjectData.ContainsKey("Moving"))
+        {
+            if(data.gameObjectData["Moving"]!=this.gameObject)
+            {
+                data.gameObjectData.Remove("Moving");
+                data.gameObjectData.Add("Moving", this.gameObject);                
+            }
+        }else if (data!=null && allowedTags.Contains(collision.gameObject.tag) && !data.gameObjectData.ContainsKey("Moving"))
         {
             data.gameObjectData.Add("Moving", this.gameObject);
         }
+    }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        ObjectData data = collision.gameObject.GetComponent<ObjectData>();
+        if (allowedTags.Contains(collision.gameObject.tag))
+        {
+           deleteMark.Add(collision.gameObject);
+        }
+        processRemoveBuffer();
+    }
+
+    void processRemoveBuffer()
+    {
+        foreach (GameObject current in deleteMark)
+        {
+            targets.Remove(current);
+            ObjectData data = current.gameObject.GetComponent<ObjectData>();
+            if(data.gameObjectData.ContainsKey("Moving") && data.gameObjectData["Moving"]==this.gameObject)
+            {
+                current.GetComponent<ObjectData>().gameObjectData.Remove("Moving");
+            }           
+        }
+        deleteMark.Clear();
     }
     private void OnDrawGizmosSelected()
     {
-        DrawArrow.ForGizmo(transform.position, moveDir);
+        DrawArrow.ForGizmo(transform.position, moveTo);
     }
 }
--- Assets/Scripts/ObjectData.cs
+++ Assets/Scripts/ObjectData.cs
@@ -7,6 +7,7 @@ public class ObjectData : MonoBehaviour
     public Dictionary<string, int> intData = new Dictionary<string, int>();
     public Dictionary<string, bool> boolData = new Dictionary<string, bool>();
     public Dictionary<string, string> stringData = new Dictionary<string, string>();
-    public Dictionary<string, float> floatData = new Dictionary<string, float>();
+    public Dictionary<string, float> floatData = new Dictionary<string, float>();   
     public Dictionary<string, GameObject> gameObjectData = new Dictionary<string, GameObject>();
+
 }
