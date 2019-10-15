721c1a30c92a8e5427f70424000fcea7747d8dff
Josh Nelson <josh.nelson1@yahoo.com>
2019-10-15 15:42:26 -0600

    Fixed syntax error

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -7,6 +7,7 @@ public class ConveyorBelt : MonoBehaviour
     [TagSelector]
     public string[] filterTags = new string[] { };
 
+    private HashSet<string> allowedTags = new HashSet<string>();
     public float moveSpeed = 1f;
     public Vector3 moveDir = Vector3.zero;//direction to move in    
     public HashSet<GameObject> targets = new HashSet<GameObject>();   
@@ -15,21 +16,35 @@ public class ConveyorBelt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(string current in filterTags)
+        {
+            allowedTags.Add(current);
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (GameObject current in targets)
+        {
+            if (current.GetComponent<ObjectData>().gameObjectData["Moving"] == this.gameObject)
+            {
+                current.transform.position = Vector3.MoveTowards(current.transform.position, new Vector3(transform.position.x + moveDir.x, current.transform.position.y, transform.position.z + moveDir.z), Time.deltaTime * moveSpeed);
+            }else if(current.GetComponent<ObjectData>().gameObjectData["Moving"]=null)
+            {
+
+            }
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         targets.Add(collision.gameObject);
         ObjectData data = collision.gameObject.GetComponent<ObjectData>();
-        if (data!=null && data.gameObjectData)
+        if (data!=null && allowedTags.Contains(collision.gameObject.tag) && data.gameObjectData.ContainsKey("Moving"))
         {
-            collision.gameObject.getc
+            data.gameObjectData.Add("Moving", this.gameObject);
         }
 
     }
