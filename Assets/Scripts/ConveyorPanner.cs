a6a66a340b529ccc2eaffc297b3964d6bed104d1
animeshbarmukh7 <32834328+animeshbarmukh7@users.noreply.github.com>
2019-10-17 17:26:17 -0600

    Add files via upload
    Conveyor Script

---
+++ Assets/Scripts/ConveyorPanner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorPanner : MonoBehaviour
+{
+
+    public float xSpeed = 0.5f;
+    public float ySpeed = 0f;
+
+
+    float xOffset;
+    float yOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xOffset = Time.time * xSpeed;
+        yOffset = Time.time * ySpeed;
+        GetComponent<Renderer>().material.mainTextureOffset=new Vector2(xOffset,yOffset);
+     
+    }
+}
