a34bb6ae0d1d67421ea6fa598a69fa0afdcb29f0
Josh Nelson <josh.nelson1@yahoo.com>
2019-10-17 12:57:32 -0600

    VR hand animations

--- Assets/_VRHands/Scripts/HandAnimator.cs
+++ Assets/_VRHands/Scripts/HandAnimator.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class HandAnimator : MonoBehaviour
 {
+    Animator anim;
+    
     private void Awake()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     private void OnDestroy()
     {
 
+    }
+
+   public void onHandCloseAmmountChange(float axis)
+    {
+        //Debug.Log(axis);
+        anim.SetFloat("GrabBlend", axis);
+        
     }
 
     private void OnTriggerEnter(Collider other)
