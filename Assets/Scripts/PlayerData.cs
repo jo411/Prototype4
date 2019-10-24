6499c488f0a8b593999938197e9ec855cd062ff7
Michael <pulis.michael@gmail.com>
2019-10-24 10:02:22 -0600

    Added base for lives system and adjusted Player prefab

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -10,15 +10,19 @@ public class PlayerData : MonoBehaviour
 
     private int totalPoints = 0;
     private float timePlayed = 0.0f;
-
+    private int lives = 3;
 
     private PlayerUI playerUI;
     private GameHandler gameHandler;
+    private ParticleSystem snowParticleSystem;
 
     private void Awake()
     {
         playerUI = GameObject.FindObjectOfType<PlayerUI>();
         gameHandler = GameObject.FindObjectOfType<GameHandler>();
+        snowParticleSystem = GameObject.Find("Snow").GetComponent<ParticleSystem>(); //TODO should probably fix
+        if (snowParticleSystem != null)
+            snowParticleSystem.Stop();
     }
 
     private void Update()
@@ -32,6 +36,24 @@ public class PlayerData : MonoBehaviour
             }
         }
     }
+
+
+    #region Restart
+    public void RestartGame()
+    {
+        totalPoints = 0;
+        timePlayed = 0.0f;
+        lives = 3; //TODO change to max lives with a const variable or modifiable value
+
+        playerUI.UpdatePoints(totalPoints);
+        playerUI.UpdateTimePlayed(timePlayed);
+        playerUI.UpdateLives(lives);
+
+        if (snowParticleSystem != null)
+            snowParticleSystem.Stop();
+    }
+
+    #endregion Restart
 
 
     #region Data Management
@@ -51,9 +73,37 @@ public class PlayerData : MonoBehaviour
     {
         totalPoints -= removedPoints;
         playerUI.UpdatePoints(totalPoints);
+
+        lives--;
+        playerUI.UpdateLives(lives);
+        AdjustEnvironment();
     }
 
     #endregion Data Management
+
+
+    #region Environment
+    /// <summary>
+    /// Adjusts the environment based on player lives
+    /// </summary>
+    public void AdjustEnvironment()
+    {
+        if(lives == 2)
+        {
+            if (snowParticleSystem != null)
+                snowParticleSystem.Play();
+        }
+        else if(lives == 1)
+        {
+
+        }
+        else if(lives == 0)
+        {
+
+        }
+    }
+
+    #endregion Environment
 
     #region Getters
     /// <summary>
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -13,6 +13,12 @@ public class PlayerUI : MonoBehaviour
     public TextMeshProUGUI TimePlayedText;
     public TextMeshProUGUI PointsText;
 
+    public CanvasGroup Heart1;
+    public CanvasGroup Heart2;
+    public CanvasGroup Heart3;
+    public CanvasGroup Heart4;
+    public CanvasGroup Heart5;
+
     public void UpdateTimePlayed(float totalTime)
     {
         var ss = Convert.ToInt32(totalTime % 60).ToString("00");
@@ -20,10 +26,43 @@ public class PlayerUI : MonoBehaviour
         TimePlayedText.text = "Time Played: " + mm + ":" + ss;
     }
 
-
+    /// <summary>
+    /// Update points in the UI
+    /// </summary>
     public void UpdatePoints(int totalPoints)
     {
         PointsText.text = "Points: " + totalPoints.ToString();
+    }
+
+    /// <summary>
+    /// Update lives in the UI
+    /// </summary>
+    public void UpdateLives(int lives)
+    {
+        if (lives >= 1)
+            Heart1.alpha = 1;
+        else
+            Heart1.alpha = 0;
+
+        if (lives >= 2)
+            Heart2.alpha = 1;
+        else
+            Heart2.alpha = 0;
+
+        if (lives >= 3)
+            Heart3.alpha = 1;
+        else
+            Heart3.alpha = 0;
+
+        if (lives >= 4)
+            Heart4.alpha = 1;
+        else
+            Heart4.alpha = 0;
+
+        if (lives >= 5)
+            Heart5.alpha = 1;
+        else
+            Heart5.alpha = 0;
     }
 
 }
