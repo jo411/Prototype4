aa709e6712668d3735c75224ddcd78cafd265cfe
Josh Nelson <josh.nelson1@yahoo.com>
2019-10-24 12:37:30 -0600

    New sounds imported

---
+++ Assets/Scripts/PlayLoopingSoundOnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayLoopingSoundOnStart : MonoBehaviour
+{
+    public AudioClip loopingSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.PlayLoop(loopingSound, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
