fe10d38c9fb363cfede1393a0450ac4d648adc9d
Josh Nelson <josh.nelson1@yahoo.com>
2019-10-24 11:58:22 -0600

    Multiplier for throw speed

--- Assets/Scripts/ItemGrabCallbacks.cs
+++ Assets/Scripts/ItemGrabCallbacks.cs
@@ -5,24 +5,32 @@ using UnityEngine.Events;
 using VRTK.Prefabs.Interactions.Interactables;
 
 [RequireComponent(typeof(InteractableFacade))]
+[RequireComponent(typeof(Rigidbody))]
 public class ItemGrabCallbacks : MonoBehaviour
 {
     public AudioClip grabSound;
+    public float throwSpeedMult;
+
+    private Rigidbody rb;
     
    
    
     public void Start()
     {
         GetComponent<InteractableFacade>().Grabbed.AddListener(delegate { onItemGrab(); });
+        GetComponent<InteractableFacade>().Ungrabbed.AddListener(delegate { onItemRelease(); });
+        rb = GetComponent<Rigidbody>();
     }
     public void onItemGrab()
     {
         AudioManager.Instance.Play(grabSound, transform);
     }
 
-    public void onItemRelease(InteractableFacade facade)
+    public void onItemRelease()
     {
         //maybe apply force multiplier here
+        rb.velocity *= throwSpeedMult;
+
     }
 }
 
